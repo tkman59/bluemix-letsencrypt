13c57f499ff9c4e38aceb07332f470e6048c8c79
Christopher Waldon <ckwaldon@us.ibm.com>
2016-08-05 10:39:35 -0400

    Change from naive wait for certs to parsing logs until ready.

--- setup-app.py
+++ setup-app.py
@@ -1,6 +1,7 @@
 import yaml
-from subprocess import call, Popen, PIPE, STDOUT
+from subprocess import call, Popen, PIPE
 import time
+
 
 def get_cert(appname, domain, certname):
     """get_cert wraps the `cf files` command to retrive only the literal file
@@ -30,7 +31,8 @@ appname = manifest['applications'][0]['name']
 # Push the app, but don't start it yet
 call(["cf", "push", "--no-start"])
 
-# For each domain, map a route for the specific letsencrypt check path '/.well-known/acme-challenge/'
+# For each domain, map a route for the specific letsencrypt check path
+# '/.well-known/acme-challenge/'
 for entry in settings['domains']:
     domain = entry['domain']
     for host in entry['hosts']:
@@ -42,11 +44,31 @@ for entry in settings['domains']:
 # Now the app can be started
 call(["cf", "start", appname])
 
+# Tail the application log
+print("Parsing log files.")
+end_token = "cf stop letsencrypt"  # Seeing this in the log means certs done
+log_pipe = Popen("cf logs %s --recent" % appname, shell=True,
+                 stdout=PIPE, stderr=PIPE)
+log_lines = log_pipe.stdout.readlines()
+print("Waiting for certs (could take several minutes)")
+while end_token not in ''.join(log_lines):
+    # Keep checking the logs for cert readiness
+    print("Certs not ready yet, retrying in 5 seconds.")
+    print("Digest: %s" % ''.join(log_lines))
+    time.sleep(5)
+    log_pipe = Popen("cf logs %s --recent" % appname, shell=True,
+                     stdout=PIPE, stderr=PIPE)
+    log_lines = log_pipe.stdout.readlines()
+# Now that certs should be ready, parse for the commands to fetch them
+command_lines = []
+for line in log_lines:
+    if "cf files" in line:
+        print(line)
+        command_lines.append(line)
+
 # Hack to wait for app to finish. Replace with parsing cf log
 domain_with_first_host = "%s.%s" % (settings['domains'][0]['hosts'][0], domain)
 print(domain_with_first_host)
-print("Waiting for certs (could take several minutes)")
-time.sleep(60)
 
 # Pull all of the certs as local files
 get_cert(appname, domain_with_first_host, 'cert.pem')
